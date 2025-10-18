400cbaba149ae34bb5d3c33d9474679fefae16ec
joaobarreto27 <joao.vito1951@gmail.com>
2025-10-18 16:14:25 -0300

    feat: adiciona módulo DatabaseWriter para gerenciar carga em banco de dados com Pyspark.

--- dags/etl/src/infrastructure/data/utils/__init__.py
+++ dags/etl/src/infrastructure/data/utils/__init__.py
@@ -1,5 +1,6 @@
 """Módulo que gerencia funções e classes reutilizáveis."""
 
 from .connect_api import ConnectAPI as ConnectAPI
+from .connect_database import ConnectionDatabase as ConnectionDatabase
 
-__all__ = ["ConnectAPI"]
+__all__ = ["ConnectAPI", "ConnectionDatabase"]
